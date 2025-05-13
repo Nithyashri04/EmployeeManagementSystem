c53fa7941d487669b19b10f9639b2f9e1bd05b68
Nithyashri04 <nithyashri.604@gmail.com>
2025-05-13 15:48:16 +0530

    Modified

--- src/main/java/com/example/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/example/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeeManagementSystemApplication {
-
+    int a=0;
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
